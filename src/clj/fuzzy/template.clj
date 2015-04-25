f75cf398be9871a5822cb64c9aa273597d117d1c
Anatoly Smolyaninov <zarkone@ya.ru>
2015-04-25 22:22:05 +0600

    remove junk..

--- src/clj/fuzzy/template.clj
+++ src/clj/fuzzy/template.clj
@@ -21,24 +21,3 @@
        (~op ~@params)))))
 
 (defmacro test-foo [] `(.log js/console "asdsa!!!!!!!!!3"))
-
-;; (expand-vars ['p1 'p2] 'data)
-; z1 (* (:value (:z1 vars)) (:weight (:z1 vars)))
-
-;; (defschema test (and p2 p3 p4)) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
